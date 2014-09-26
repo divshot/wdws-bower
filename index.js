d468aac59c419bf7f25720f71450f5829dbb784e
Michael Bleigh <mbleigh@mbleigh.com>
2014-09-25 23:40:07 -0400

    Adds log message output.

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 var bower = require("bower");
 
 module.exports = function() { return function(server, cmd) {
-    
+  var config = {
+    cwd: server.provider.root
+  };
+  
   cmd('install', function(params, fn) {
-    bower.commands.install(params.packages, {save: params.save}).on('end', function(installed) {
+    var socket = this;
+    
+    bower.commands.install(params.packages, {save: params.save}, config).on('end', function(installed) {
+      console.log('ended')
       fn(null, installed);
     }).on('error', function(err) {
       fn(err);
+    }).on('log', function(log) {
+      console.log(">>> bower.install:log", log.message);
+      socket.pm(params.pid, 'log', log.message);
     });
   });
     
